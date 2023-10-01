2c42dd0a1db6f46b5c6a1e573942cad2cd784858
bear <maphick@yandex.ru>
2023-10-01 21:59:15 +0300

    Initial commit

---
+++ app/src/main/java/com/example/compose_0/MainActivity.kt
@@ -0,0 +1,92 @@
+package com.example.compose_0
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.compose_0.ui.theme.Compose_0Theme
+import com.example.compose_0.ui.theme.InstagramProfileCard
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Compose_0Theme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                   InstagramProfileCard()
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun TestText() {
+    Column {
+        Text(
+            text ="fgvdfsbfdsgbfgsbfgbfgb",
+            fontSize = 16.sp,
+            fontWeight = FontWeight.ExtraBold,
+            fontFamily = FontFamily.SansSerif,
+            textDecoration = TextDecoration.combine(listOf(TextDecoration.LineThrough, TextDecoration.Underline))
+        )
+        Text(
+            buildAnnotatedString {
+                withStyle(SpanStyle(fontWeight = FontWeight.Bold)) {
+                    append("Hello")
+                }
+                withStyle(SpanStyle(textDecoration = TextDecoration.Underline)) {
+                    append(" ")
+                }
+                withStyle(
+                    SpanStyle(
+                        fontSize = 30.sp,
+                        textDecoration = TextDecoration.LineThrough
+                    )
+                ) {
+                    append("World")
+                }
+                append("!")
+
+            }
+        )
+
+    }
+}
---
+++ app/src/main/java/com/example/compose_0/ui/theme/InstagramProfileCard.kt
@@ -0,0 +1,88 @@
+package com.example.compose_0.ui.theme
+
+import android.icu.text.CaseMap.Title
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+@Preview
+@Composable
+fun InstagramProfileCard() {
+    Card(
+        shape = RoundedCornerShape(
+            topStart = 4.dp,
+            topEnd = 4.dp),
+        colors = CardDefaults.cardColors(
+            //containerColor = Color.Green,
+            contentColor = Color.White
+        ),
+        border = BorderStroke(1.dp, Color.Black)
+    )
+    {
+    Row (
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically
+    )
+    {
+        Box(
+            modifier = Modifier
+                .size(50.dp)
+                .background(Color.Green)
+        )
+        UserStatistics("Posts", "6,950")
+        UserStatistics("Followers", "436M")
+        UserStatistics("Following", "76")
+    }
+    }
+}
+
+
+
+
+@Composable
+fun UserStatistics(
+    title: String,
+    value: String
+)
+{
+    Column(
+        modifier = Modifier
+            .height(80.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly,
+    )
+    {
+         Text(
+            text = value,
+            fontSize = 25.dp,
+             fontFamily = FontFamily.Cursive
+        )
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
